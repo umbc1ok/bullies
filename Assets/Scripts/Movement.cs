3433c62ac6ded9e6bd8bec536c25932f2d520003
umbc1ok <umbcio@gmail.com>
2023-11-24 02:27:20 +0100

    changes to movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [HideInInspector] public Controls playerControls;
+    [HideInInspector]
+    public Controls playerControls;
     Rigidbody2D rb;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerControls = new Controls();
-        playerControls.PlayerControls.Movement.performed += ctx => UpdateSpeed(ctx.ReadValue<Vector2>());
-        playerControls.PlayerControls.Movement.canceled += ctx => UpdateSpeed(Vector2.zero);
+        //playerControls = new Controls();
+        //playerControls.PlayerControls.Movement.performed += ctx => UpdateSpeed(ctx.ReadValue<Vector2>());
+        //playerControls.PlayerControls.Movement.canceled += ctx => UpdateSpeed(Vector2.zero);
     }
 
     private void FixedUpdate()
@@ -26,20 +28,12 @@ public class Movement : MonoBehaviour
         rb.MovePosition(rb.position + rb.velocity);
     }
 
-    public void UpdateSpeed(Vector2 newSpeed)
+    public void UpdateSpeed(InputAction.CallbackContext ctx)
     {
         //movementDirection = newSpeed;
-        rb.velocity = newSpeed;
+        rb.velocity = ctx.ReadValue<Vector2>();
     }
 
 
-    void OnEnable()
-    {
-        playerControls.Enable();
-    }
-    void OnDisable()
-    {
-        playerControls.Disable();
-    }
 
 }
